24fa47cbe8c2f2e17412357a1070003b25f00321
cllown <artoorclownxd@gmail.com>
2025-01-31 23:39:23 +0200

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { StarWarsComponent } from './components/star-wars/star-wars.component';
-import {CharactersPageComponent} from './components/characters-page/characters-page.component';
-
-export const appRoutes: Routes = [
-  { path: '', component: StarWarsComponent },
-  { path: 'characters', component: CharactersPageComponent },
-];
